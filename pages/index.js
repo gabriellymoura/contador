3bdf221229f22a6d25b9cbf0f765aaa9916fbd8d
Gabrielly Moura <gabriellymouta@gmail.com>
2020-11-21 10:17:17 -0300

    Add Caps Lock

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,13 @@ import {useState} from 'react';
 function Home(){
     return(
         <div className = {styles.center}>
-           <Contador/> 
+            <div>
+                <Contador/> 
+            </div>
+            <div>
+                <Input/>
+            </div>
+           
         </div>
         
     ) 
@@ -24,5 +30,51 @@ function Contador(){
         </div>
     )
 }
+
+
+class Input extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            nome: ""
+        };
+
+        this.onChange = (evento) =>{
+            this.setState({nome: evento.target.value});
+        };
+    }
+
+    render(){
+        return(
+            <div>
+               <div className = 'text'>
+                    Seu texto:
+                    <br/>
+                </div>
+                <div>         
+                    <input className='input' name = 'nome' value = {this.state.nome} onChange = {this.onChange} type = 'text'/>
+                </div>
+                <div className = 'text'>
+                    Texto em CapsLock:
+                </div>
+                <div className = 'value'>
+                <br/>
+                    <CapsLock >{this.state.nome}</CapsLock>
+                </div>
+                
+            </div>
+        )
+    }
+
+}
+
+
+function CapsLock(props){ 
+    const texto_input = props.children;
+    const texto_output = texto_input.toUpperCase();
+    return <div> {texto_output} </div>
+}
+
+
 
 export default Home;
